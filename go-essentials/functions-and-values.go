a08a8cb3b6021a7392ff71820636e19fc8989c68
Lucas-E <lucasgitacc@gmail.com>
2024-04-17 21:26:27 -0300

    adding functions and values file

---
+++ go-essentials/functions-and-values.go
@@ -0,0 +1,15 @@
+// Isso é a declaração de um package
+// Um package é um conjunto de arquivos que estão na mesma pasta
+// O Package main é especial, ele é o package que indica que o arquivo é um executável e onde o projeto começa
+package main
+
+//Isto é um package
+//Um package é um conjunto de arquivos que estão na mesma pasta
+import "fmt"
+
+// Isto é uma função
+// Uma função é um bloco de código que pode ser chamado
+// A função main é especial, ela é a função que é chamada quando o programa é executado
+func main() {
+	fmt.Println("This line is being executed!")
+}
